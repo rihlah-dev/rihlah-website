ee7686354dda215b22fd45f5496f6a1073de2375
taha-esti <taha.silat@esti.ca>
2026-02-20 10:39:58 -0600

    Updated astro config to work with cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from 'astro/integrations/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
